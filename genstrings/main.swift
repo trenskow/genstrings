8b8f611824ef88912826c037a22efc11d0de47cd
Kristian Trenskow <trenskow@me.com>
2017-04-03 00:51:36 +0200

    Initial Commit

---
+++ genstrings/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  genstrings
+//
+//  Created by Kristian Trenskow on 03/04/2017.
+//  Copyright © 2017 trenskow. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
