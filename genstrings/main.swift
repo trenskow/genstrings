23a9dea9bc9fb9295a4b7933688a9eb8b7e10add
Kristian Trenskow <trenskow@me.com>
2017-04-03 02:41:39 +0200

    First commit.

--- genstrings/main.swift
+++ genstrings/main.swift
@@ -1,3 +1,4 @@
+#!/usr/bin/swift
 //
 //  main.swift
 //  genstrings
@@ -8,5 +9,64 @@
 
 import Foundation
 
-print("Hello, World!")
+let exp = try! NSRegularExpression(pattern: "(?<=\")([^\"]*)(?=\".(localize\\((\\\"(.*?)\\\")?\\)))", options: [])
 
+func printErr(_ string: String) {
+    let stderr = FileHandle.standardError
+    stderr.write("\(string)\n".data(using: .utf8)!)
+}
+
+func findFiles(path: String) throws -> [String] {
+    return try FileManager.default.contentsOfDirectory(atPath: path).reduce([], { (result, subpath) -> [String] in
+        var isDirectory: ObjCBool = false
+        guard FileManager.default.fileExists(atPath: "\(path)/\(subpath)", isDirectory: &isDirectory) else { return result }
+        guard isDirectory.boolValue == false else {
+            return try result + findFiles(path: "\(path)/\(subpath)")
+        }
+        return result + ["\(path)/\(subpath)"]
+    }).filter({ (path) -> Bool in
+        path.contains(".swift")
+    })
+}
+
+var path: String? = nil
+
+if CommandLine.arguments.count > 1 {
+    path = CommandLine.arguments[1]
+}
+
+
+try findFiles(path: path ?? FileManager.default.currentDirectoryPath)
+.map { (path) -> [(String, String?)] in
+    
+    let data = try! Data(contentsOf: URL(fileURLWithPath: path))
+    
+    let string = String(data: data, encoding: .utf8)!
+    
+    return exp.matches(in: string, options: [], range: NSMakeRange(0, string.characters.count)).reduce([], { (ret, result) -> [(String, String?)] in
+        
+        let found = (0 ..< result.numberOfRanges).map({ (idx) -> String? in
+            let range = result.rangeAt(idx)
+            guard range.location != NSNotFound else { return nil }
+            let startIndex = string.index(string.startIndex, offsetBy: range.location)
+            let endIndex = string.index(startIndex, offsetBy: range.length)
+            return string.substring(with: startIndex ..< endIndex)
+        })
+        
+        return ret + [(found.first!!, found.last!)]
+        
+    })
+    
+}
+.reduce([], +)
+.reduce([]) { (result, strings) -> [(String, String?)] in
+    guard !result.contains(where: { $0.0 == strings.0 }) else { return result }
+    return result + [strings]
+}
+.forEach { (strings) in
+    let string = strings.0.components(separatedBy: "\"").joined(separator: "\\\"")
+    let comment = strings.1 ?? "No comment provided by engineer"
+    print()
+    print("/* \(comment) */")
+    print("\"\(string)\" = \"\(string)\";")
+}
