55bb411fb4e2db8eec42330172136ebd7dd0dc0c
Kristian Trenskow <trenskow@me.com>
2017-04-03 10:09:49 +0200

    Fixed indentation.

--- genstrings/main.swift
+++ genstrings/main.swift
@@ -30,38 +30,39 @@ if CommandLine.arguments.count > 1 {
     path = CommandLine.arguments[1]
 }
 
+FileHandle.standardOutput.write(Data(bytes: [0xfe, 0xff]))
 FileHandle.standardOutput.write(try findFiles(path: path ?? FileManager.default.currentDirectoryPath)
-.map { (path) -> [(String, String?)] in
-    
-    let data = try! Data(contentsOf: URL(fileURLWithPath: path))
-    
-    let string = String(data: data, encoding: .utf8)!
-    
-    return exp.matches(in: string, options: [], range: NSMakeRange(0, string.characters.count)).reduce([], { (ret, result) -> [(String, String?)] in
+    .map { (path) -> [(String, String?)] in
         
-        let found = (0 ..< result.numberOfRanges).map({ (idx) -> String? in
-            let range = result.rangeAt(idx)
-            guard range.location != NSNotFound else { return nil }
-            let startIndex = string.index(string.startIndex, offsetBy: range.location)
-            let endIndex = string.index(startIndex, offsetBy: range.length)
-            return string.substring(with: startIndex ..< endIndex)
+        let data = try! Data(contentsOf: URL(fileURLWithPath: path))
+        
+        let string = String(data: data, encoding: .utf8)!
+        
+        return exp.matches(in: string, options: [], range: NSMakeRange(0, string.characters.count)).reduce([], { (ret, result) -> [(String, String?)] in
+            
+            let found = (0 ..< result.numberOfRanges).map({ (idx) -> String? in
+                let range = result.rangeAt(idx)
+                guard range.location != NSNotFound else { return nil }
+                let startIndex = string.index(string.startIndex, offsetBy: range.location)
+                let endIndex = string.index(startIndex, offsetBy: range.length)
+                return string.substring(with: startIndex ..< endIndex)
+            })
+            
+            return ret + [(found.first!!, found.last!)]
+            
         })
         
-        return ret + [(found.first!!, found.last!)]
-        
-    })
-    
-}
-.reduce([], +)
-.reduce([]) { (result, strings) -> [(String, String?)] in
-    guard !result.contains(where: { $0.0 == strings.0 }) else { return result }
-    return result + [strings]
-}
-.map { (strings) in
-    let string = strings.0.components(separatedBy: "\"").joined(separator: "\\\"")
-    let comment = strings.1 ?? "No comment provided by engineer"
-    return ["", "/* \(comment) */", "\"\(string)\" = \"\(string)\";"]
-}
-.reduce([], +)
-.joined(separator: "\n")
-.data(using: .utf16BigEndian)!)
+    }
+    .reduce([], +)
+    .reduce([]) { (result, strings) -> [(String, String?)] in
+        guard !result.contains(where: { $0.0 == strings.0 }) else { return result }
+        return result + [strings]
+    }
+    .map { (strings) in
+        let string = strings.0.components(separatedBy: "\"").joined(separator: "\\\"")
+        let comment = strings.1 ?? "No comment provided by engineer"
+        return ["", "/* \(comment) */", "\"\(string)\" = \"\(string)\";"]
+    }
+    .reduce([], +)
+    .joined(separator: "\n")
+    .data(using: .utf16BigEndian)!)
